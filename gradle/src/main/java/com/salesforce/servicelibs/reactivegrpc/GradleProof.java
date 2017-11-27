f524e29917d9ad0691e7328550b3e88164e8dee3
Ryan Michela <deltahat@gmail.com>
2017-11-26 20:55:08 -0800

    Simple HelloWorld

---
+++ gradle/src/main/java/com/salesforce/servicelibs/reactivegrpc/GradleProof.java
@@ -0,0 +1,55 @@
+package com.salesforce.servicelibs.reactivegrpc;
+
+import io.grpc.ManagedChannel;
+import io.grpc.Server;
+import io.grpc.inprocess.InProcessChannelBuilder;
+import io.grpc.inprocess.InProcessServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+public class GradleProof extends GreeterGrpc.GreeterImplBase {
+    public static void main(String[] args) throws Exception {
+        GradleProof proof = new GradleProof();
+        try {
+            proof.startServer();
+            System.out.println(proof.doClient("World"));
+        } finally {
+            proof.stopServer();
+        }
+    }
+
+    private Server server;
+
+    public void startServer() throws Exception {
+        server = InProcessServerBuilder
+                .forName("GradleProof")
+                .addService(this)
+                .build()
+                .start();
+    }
+
+    public void stopServer() {
+        if (server != null) {
+            server.shutdownNow();
+        }
+    }
+
+    public String doClient(String name) {
+        ManagedChannel channel = InProcessChannelBuilder
+                .forName("GradleProof")
+                .build();
+        try {
+            GreeterGrpc.GreeterBlockingStub stub = GreeterGrpc.newBlockingStub(channel);
+            HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+            HelloResponse response = stub.sayHello(request);
+            return response.getMessage();
+        } finally {
+            channel.shutdownNow();
+        }
+    }
+
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        responseObserver.onNext(HelloResponse.newBuilder().setMessage("Hello " + request.getName()).build());
+        responseObserver.onCompleted();
+    }
+}
