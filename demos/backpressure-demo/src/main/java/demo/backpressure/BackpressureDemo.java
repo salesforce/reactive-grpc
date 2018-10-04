ce23fec98fa573396d1359ae7d2c32b43f5a1945
Ryan Michela <rmichela@salesforce.com>
2018-10-04 16:03:57 -0700

    Make backpressure demo executable

--- demos/backpressure-demo/src/main/java/demo/backpressure/BackpressureDemo.java
+++ demos/backpressure-demo/src/main/java/demo/backpressure/BackpressureDemo.java
@@ -14,7 +14,7 @@ public class BackpressureDemo extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Backpressure.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Backpressure.fxml"));
 
         stage.setTitle("Reactive-gRPC Backpressure Demo");
         stage.setScene(new Scene(root, 800, 450));
