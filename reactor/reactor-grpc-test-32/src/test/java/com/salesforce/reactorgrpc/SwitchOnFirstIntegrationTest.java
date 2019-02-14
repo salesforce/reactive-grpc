4c82812482ef36907fc7e7f35f56e286a49975f4
Ryan Michela <rmichela@salesforce.com>
2019-02-13 21:15:18 -0800

    Formatting and documentation

--- reactor/reactor-grpc-test-32/src/test/java/com/salesforce/reactorgrpc/SwitchOnFirstIntegrationTest.java
+++ reactor/reactor-grpc-test-32/src/test/java/com/salesforce/reactorgrpc/SwitchOnFirstIntegrationTest.java
@@ -11,6 +11,8 @@ import reactor.test.StepVerifier;
 /**
  * Verifies the Flux SwitchOnFirst operator works correctly. Derived from demonstration code at
  * https://github.com/simonbasle/reactiveGrpcSwitchOnFirst
+ *
+ * https://github.com/salesforce/reactive-grpc/issues/136
  */
 public class SwitchOnFirstIntegrationTest {
     @Rule
@@ -20,26 +22,26 @@ public class SwitchOnFirstIntegrationTest {
         @Override
         public Flux<Greeting> skipGreet(Flux<Frame> request) {
             return request
-                    .doOnNext(frame -> System.out.println("input from client: " + frame.toString().replaceAll("\n", "\t")))
-                    .switchOnFirst((firstSignal, all) -> {
-                        System.out.println("switching on first signal " + firstSignal); //FIXME this never gets invoked
-                        if (firstSignal.isOnNext()) {
-                            Frame frame = firstSignal.get();
-                            assert frame != null;
-                            if (frame.hasConfig()) {
-                                int skip = frame.getConfig().getSkip();
-                                return all.filter(Frame::hasPayload).skip(skip).map(Frame::getPayload);
-                            }
-                            else {
-                                //no configuration frame
-                                return Mono.error(new IllegalArgumentException("Missing Config frame at start"));
-                            }
-                        } else {
-                            //avoid returning Flux.empty, which would suppress an immediate error.
-                            return all.cast(Payload.class); //the "all" input Flux completes or errors immediately, so no real payload
+                .doOnNext(frame -> System.out.println("input from client: " + frame.toString().replaceAll("\n", "\t")))
+                .switchOnFirst((firstSignal, all) -> {
+                    System.out.println("switching on first signal " + firstSignal); //FIXME this never gets invoked
+                    if (firstSignal.isOnNext()) {
+                        Frame frame = firstSignal.get();
+                        assert frame != null;
+                        if (frame.hasConfig()) {
+                            int skip = frame.getConfig().getSkip();
+                            return all.filter(Frame::hasPayload).skip(skip).map(Frame::getPayload);
                         }
-                    })
-                    .map(payload -> Greeting.newBuilder().setMessage("Hello " + payload.getName()).build());
+                        else {
+                            //no configuration frame
+                            return Mono.error(new IllegalArgumentException("Missing Config frame at start"));
+                        }
+                    } else {
+                        //avoid returning Flux.empty, which would suppress an immediate error.
+                        return all.cast(Payload.class); //the "all" input Flux completes or errors immediately, so no real payload
+                    }
+                })
+                .map(payload -> Greeting.newBuilder().setMessage("Hello " + payload.getName()).build());
         }
     };
 
