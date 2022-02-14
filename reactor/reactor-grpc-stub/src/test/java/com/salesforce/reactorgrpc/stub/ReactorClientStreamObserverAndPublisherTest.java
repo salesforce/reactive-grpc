0601bfcfebf24bfcef0cc9024e2891d2c2dead82
Kirill Rodionov <bobruto+github@gmail.com>
2022-02-14 22:30:35 +0700

    fixes https://github.com/salesforce/reactive-grpc/issues/292 (supposedly)

--- reactor/reactor-grpc-stub/src/test/java/com/salesforce/reactorgrpc/stub/ReactorClientStreamObserverAndPublisherTest.java
+++ reactor/reactor-grpc-stub/src/test/java/com/salesforce/reactorgrpc/stub/ReactorClientStreamObserverAndPublisherTest.java
@@ -6,17 +6,23 @@
 
 package com.salesforce.reactorgrpc.stub;
 
-import java.util.concurrent.ForkJoinPool;
-
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.Fuseable;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class ReactorClientStreamObserverAndPublisherTest {
+    private static final Logger log = LoggerFactory.getLogger(ReactorClientStreamObserverAndPublisherTest.class.getName());
 
     private static final int DEFAULT_CHUNK_SIZE = 512;
     private static final int PART_OF_CHUNK = DEFAULT_CHUNK_SIZE * 2 / 3;
@@ -24,7 +30,7 @@ public class ReactorClientStreamObserverAndPublisherTest {
     @Test
     public void multiThreadedProducerTest() {
         ReactorClientStreamObserverAndPublisher<Integer> processor =
-                new ReactorClientStreamObserverAndPublisher<>(null);
+            new ReactorClientStreamObserverAndPublisher<>(null);
         int countPerThread = 100000;
         TestCallStreamObserverProducer observer =
             new TestCallStreamObserverProducer(ForkJoinPool.commonPool(), processor, countPerThread);
@@ -34,21 +40,76 @@ public class ReactorClientStreamObserverAndPublisherTest {
                     .expectNextCount(countPerThread)
                     .verifyComplete();
 
-        Assertions.assertThat(observer.requestsQueue.size()).isBetween((countPerThread - DEFAULT_CHUNK_SIZE) / PART_OF_CHUNK + 1, (countPerThread - DEFAULT_CHUNK_SIZE) / PART_OF_CHUNK + 3);
+        assertThat(observer.requestsQueue.size()).isBetween((countPerThread - DEFAULT_CHUNK_SIZE) / PART_OF_CHUNK + 1,
+            (countPerThread - DEFAULT_CHUNK_SIZE) / PART_OF_CHUNK + 3);
     }
 
     @Test
     public void producerFusedTest() {
         ReactorClientStreamObserverAndPublisher<Integer> processor =
-                new ReactorClientStreamObserverAndPublisher<>(null);
+            new ReactorClientStreamObserverAndPublisher<>(null);
         int countPerThread = 100000;
-        TestCallStreamObserverProducer observer = new TestCallStreamObserverProducer(ForkJoinPool.commonPool(), processor, countPerThread);
+        TestCallStreamObserverProducer observer = new TestCallStreamObserverProducer(ForkJoinPool.commonPool(),
+            processor, countPerThread);
         processor.beforeStart(observer);
         StepVerifier.create(Flux.from(processor))
                     .expectFusion(Fuseable.ANY, Fuseable.ASYNC)
                     .expectNextCount(countPerThread)
                     .verifyComplete();
 
-        Assertions.assertThat(observer.requestsQueue.size()).isBetween((countPerThread - DEFAULT_CHUNK_SIZE) / PART_OF_CHUNK + 1, (countPerThread - DEFAULT_CHUNK_SIZE) / PART_OF_CHUNK + 3);
+        assertThat(observer.requestsQueue.size()).isBetween((countPerThread - DEFAULT_CHUNK_SIZE) / PART_OF_CHUNK + 1,
+            (countPerThread - DEFAULT_CHUNK_SIZE) / PART_OF_CHUNK + 3);
+    }
+
+    @Test
+    public void discardQueueTest() {
+        ReactorClientStreamObserverAndPublisher<Integer> processor =
+            new ReactorClientStreamObserverAndPublisher<>(null);
+        int countPerThread = 5;
+        TestCallStreamObserverProducer observer = new TestCallStreamObserverProducer(ForkJoinPool.commonPool(),
+            processor, countPerThread);
+        processor.beforeStart(observer);
+
+        ConcurrentLinkedQueue<Integer> discardedByObserverAndPublisher = new ConcurrentLinkedQueue<>();
+        ConcurrentLinkedQueue<Integer> discardedByPublishOn = new ConcurrentLinkedQueue<>();
+
+        AtomicBoolean firstHandled = new AtomicBoolean();
+        Flux<Integer> consumer =
+            Flux.from(processor)
+                .doOnDiscard(Integer.class, i -> {
+                    log.info("Processor: discarding {}", i);
+                    discardedByObserverAndPublisher.add(i);
+                })
+                .log("processor")
+                .limitRate(1)
+                .publishOn(Schedulers.parallel())
+                .limitRate(1)
+                .doOnDiscard(Integer.class, i -> {
+                    log.info("publishOn: discarding {}", i);
+                    discardedByPublishOn.add(i);
+                })
+                .<Integer>handle((i, sink) -> {
+                    if (firstHandled.compareAndSet(false, true)) {
+                        try {
+                            Thread.sleep(100);
+                        } catch (Exception e) {
+                            // noop
+                        }
+                        sink.next(i);
+                    } else {
+                        sink.complete();
+                    }
+                })
+                .log("handled");
+
+        StepVerifier.create(consumer)
+                    .expectNext(0)
+                    .verifyComplete();
+
+        // 1 is dropped in handle without invoking the discard hook,
+        assertThat(discardedByObserverAndPublisher).containsExactly(3, 4);
+        // impl details: processor is able to schedule 2 before it's cancelled
+        // also, discard hooks are cumulative, so not using containsExactly
+        assertThat(discardedByPublishOn).contains(2);
     }
 }
